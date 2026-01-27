4c29555396f6b684dd9704a3d96f8ae0176bb3c3
anagha.kulkarni@cumminscollege.in <anagha.kulkarni@cumminscollege.in>
2026-01-27 15:55:03 +0530

    modified:   gradeCalCTest.java

--- LocalRepo/gradeCalCTest.java
+++ LocalRepo/gradeCalCTest.java
@@ -1,7 +1,7 @@
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 
 public class gradeCalCTest {
@@ -35,7 +35,28 @@ public class gradeCalCTest {
         //gradeCalc grCalculation = new gradeCalc();
         Assert.assertEquals("A", grCalculation.grade(79));
     }
-
-
-
+    public void testforB() {
+        //gradeCalc grCalculation = new gradeCalc();
+        Assert.assertEquals("B", grCalculation.grade(69));
+    }
+    public void testforC() {
+        //gradeCalc grCalculation = new gradeCalc();
+        Assert.assertEquals("C", grCalculation.grade(58));
+    }
+    public void testforD() {
+        //gradeCalc grCalculation = new gradeCalc();
+        Assert.assertEquals("D", grCalculation.grade(45));
+    }
+    public void testforFBorderCase() {
+        //gradeCalc grCalculation = new gradeCalc();
+        Assert.assertEquals("F", grCalculation.grade(39));
+    }
+    public void testforF() {
+        //gradeCalc grCalculation = new gradeCalc();
+        Assert.assertEquals("F", grCalculation.grade(10));
+    }
+    public void testforFZero() {
+        //gradeCalc grCalculation = new gradeCalc();
+        Assert.assertEquals("F", grCalculation.grade(0));
+    }
 }
